e19d33d5cf65861319fff67f6087e9cf85eeff24
shubhammehra4 <shubhammehra4@gmail.com>
2021-02-25 16:08:15 +0530

    Priority Queue + Min & Max Heap

--- Data Structures/Priority Queue STL.cpp
+++ Data Structures/Priority Queue STL.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef pair<int, int> iPair;
+
 void showpq (priority_queue <int> gq)
 {
     priority_queue <int> g = gq;
@@ -14,47 +16,59 @@ void showpq (priority_queue <int> gq)
     cout << '\n';
 }
 
-void showpqmin (priority_queue <int, vector<int>, greater<int>> gq)
-{
-    priority_queue <int, vector<int>, greater<int>> g = gq;
-
-    while (!g.empty() )
-    {
-        cout << '\t' << g.top();
-        g.pop();
-    }
-
-    cout << '\n';
-}
-
 int main ()
 {
-    priority_queue <int> hello;
-    hello.push (10);
-    hello.push (30);
-    hello.push (20);
-    hello.push (5);
-    hello.push (1);
-    cout << "The priority queue hello is : ";
-    showpq (hello);
-    cout << "\nhello.size() : " << hello.size();
-    cout << "\nhello.top() : " << hello.top();
-    cout << "\nhello.pop() : ";
-    hello.pop();
-    showpq (hello);
-    priority_queue <int, vector<int>, greater<int>> gquiz;
-    gquiz.push (10);
-    gquiz.push (30);
-    gquiz.push (20);
-    gquiz.push (5);
-    gquiz.push (1);
-    cout << "The priority queue gquiz is : ";
-    showpqmin (gquiz);
-    cout << "\ngquiz.size() : " << gquiz.size();
-    cout << "\ngquiz.top() : " << gquiz.top();
-    cout << "\ngquiz.pop() : ";
-    gquiz.pop();
-    showpqmin (gquiz);
+    priority_queue <int> pq;
+    pq.push (10);
+    pq.push (30);
+    pq.push (20);
+    pq.push (5);
+    pq.push (1);
+    cout << "The priority queue pq is : ";
+    showpq (pq);
+    cout << "pq.size() : " << pq.size();
+    cout << "\npq.top() : " << pq.top();
+    cout << "\npq.pop() : ";
+    pq.pop();
+    showpq (pq);
+    // Min - Heap
+    priority_queue< iPair, vector <iPair> , greater<iPair> > minHeap;
+    minHeap.push ({10, 2});
+    minHeap.push ({30, 3});
+    minHeap.push ({20, 5});
+    minHeap.push ({5, 1});
+    minHeap.push ({6, 8});
+    cout << endl;
+    cout << "The priority queue minHeap is : ";
+    cout << "\nminHeap.top() : " << minHeap.top().first << " - " << minHeap.top().second;
+    cout << "\nminHeap.size() : " << minHeap.size();
+    cout << endl;
+
+    while (!minHeap.empty() )
+    {
+        cout << minHeap.top().first << " - " << minHeap.top().second << endl;
+        minHeap.pop();
+    }
+
+    // Max-Heap
+    priority_queue <iPair> maxHeap;
+    maxHeap.push ({10, 2});
+    maxHeap.push ({30, 3});
+    maxHeap.push ({20, 5});
+    maxHeap.push ({5, 1});
+    maxHeap.push ({6, 8});
+    cout << endl;
+    cout << "The priority queue maxHeap is : ";
+    cout << "\nmaxHeap.top() : " << maxHeap.top().first << " - " << maxHeap.top().second;
+    cout << "\nmaxHeap.size() : " << maxHeap.size();
+    cout << endl;
+
+    while (!maxHeap.empty() )
+    {
+        cout << maxHeap.top().first << " - " << maxHeap.top().second << endl;
+        maxHeap.pop();
+    }
+
     return 0;
 }
 
