eb38ff3ad5d578b0be4295ab2c368cca7b28c139
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-22 18:30:42 +0530

    Interval Scheduling

--- Algorithms/Interval Scheduling.cpp
+++ Algorithms/Interval Scheduling.cpp
@@ -37,17 +37,38 @@ void intervalSchedule (Interval arr[], int n) // Naive Approach
 
     for (auto i = res.begin(); i != res.end(); i++)
     {
-        cout << arr[*i].start << " " << arr[*i].end << endl;
+        cout << arr[*i].start << " - " << arr[*i].end << endl;
+    }
+}
+
+void intervalScheduleOpti (Interval arr[], int n)
+{
+    int st[n];
+
+    for (int i = 0; i < n; i++)
+        st[i] = arr[i].start;
+
+    cout << "Jobs are:" << endl;
+    cout << arr[0].start << " - " << arr[0].end << endl;
+    int i = arr[0].end;
+
+    for (int j = 1; j < n; j++)
+    {
+        if (st[j] >= i)
+        {
+            cout << arr[j].start << " - " << arr[j].end << endl;
+            i = arr[j].end;
+        }
     }
 }
 
 
 int main()
 {
-    Interval arr[]
-    = { { 0, 6 }, { 7, 8 }, { 0, 1 }, { 2, 3 }, {4, 5}, {6, 9}, {0, 3}, {4, 6}, {6, 7}};
+    Interval arr[] = { { 0, 6 }, { 7, 8 }, { 0, 1 }, { 2, 3 }, {4, 5}, {6, 9}, {0, 3}, {4, 6}, {6, 7}};
     int n = sizeof (arr) / sizeof (arr[0]);
-    sort (arr, arr + n, compareInterval);
+    sort (arr, arr + n, compareInterval); // Sort According to Ending
     intervalSchedule (arr, n);
+    intervalScheduleOpti (arr, n);
     return 0;
 }
