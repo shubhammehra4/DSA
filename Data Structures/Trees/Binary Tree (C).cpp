d4221fc570bea954cddb6ce7bdc27c8b52cfdda2
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-22 23:54:36 +0530

    Binary Tree Basic C

--- Data Structures/Trees/Binary Tree (C).cpp
+++ Data Structures/Trees/Binary Tree (C).cpp
@@ -1,32 +1,3 @@
-// #include <bits/stdc++.h>
-
-// struct node
-// {
-//     int data;
-//     struct node *left;
-//     struct node *right;
-// };
-
-// struct node* newNode (int data)
-// {
-//     struct node* node = (struct node*) malloc (sizeof (struct node) );
-//     node->data = data;
-//     node->left = NULL;
-//     node->right = NULL;
-//     return (node);
-// }
-
-
-// int main()
-// {
-//     struct node *root = newNode (1);
-//     root->left   = newNode (2);
-//     root->right  = newNode (3);
-//     root->left->left = newNode (4);
-//     getchar();
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 #define SIZE 50
 
