1bd70bccb33010b67c17de165136bb40765d1fa6
shubhammehra4 <shubhammehra4@gmail.com>
2021-04-09 12:30:56 +0530

    Grid Traveller DP

--- Algorithms/gridTraveller.cpp
+++ Algorithms/gridTraveller.cpp
@@ -17,7 +17,7 @@ ll memoGridTraveller (ll m , ll n) // Time:O(m*n) Space: O(m+n)
 {
     if (m == 0 || n == 0) return 0;
 
-    string key = to_string (m) + "," + to_string (n);
+    string key = to_string (m) + "," + to_string (n); // deniting a node as a key
 
     if (memo.find (key)->second) return memo[key];
 
@@ -27,13 +27,34 @@ ll memoGridTraveller (ll m , ll n) // Time:O(m*n) Space: O(m+n)
     return memo[key];
 }
 
+ll dynamicGridTraveller (ll m, ll n)
+{
+    vector<vector<int>> paths (m, vector<int> (n, 0) );
+    paths[0][n - 1] = 1; //starting
+
+    for (int i = n - 2; i >= 0; i--)
+        paths[0][i] = paths[0][i + 1]; // left side
+
+    for (int i = 1; i < m; i++)
+        paths[i][n - 1] = paths[i - 1][n - 1]; // top side
+
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = n - 2; j >= 0; j--)
+            paths[i][j] = paths[i - 1][j] + paths[i][j + 1];
+    }
+
+    return paths[m - 1][0];
+}
+
 int main()
 {
     cout << memoGridTraveller (1, 1) << endl;
-    cout << memoGridTraveller (2, 3) << endl;
     cout << memoGridTraveller (3, 2) << endl;
-    cout << memoGridTraveller (3, 3) << endl;
     cout << memoGridTraveller (18, 18) << endl;
+    cout << dynamicGridTraveller (18, 18) << endl;
+    cout << memoGridTraveller (15, 15) << endl;
     cout << gridTraveller (15, 15) << endl;
+    cout << dynamicGridTraveller (15, 15) << endl;
     return 0;
 }
