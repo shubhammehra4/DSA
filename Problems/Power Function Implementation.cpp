4b95aa17c22af6a7d881bb63ae261e49b15cedb8
shubhammehra4 <shubhammehra4@gmail.com>
2021-04-21 13:00:04 +0530

    Power Function

---
+++ Problems/Power Function Implementation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+int pow (int x, int n, int d)
+{
+    if (d == 1)
+        return 0;
+
+    if (x == 0)
+        return 0;
+
+    ll ans = 1, y = x % d;
+
+    while (n > 0)
+    {
+        if (n & 1) // odd
+            ans = ( (ans) * y) % d;
+
+        y = (y * y) % d;
+
+        if (y < 0)
+            y += d;
+
+        n = n >> 1; // n/2
+    }
+
+    if (ans < 0)
+    {
+        ans = (d - abs (ans) % d);
+        return ans;
+    }
+
+    return ans;
+}
+
+
+int main()
+{
+    cout << pow (2, 6, 3);
+    return 0;
+}
