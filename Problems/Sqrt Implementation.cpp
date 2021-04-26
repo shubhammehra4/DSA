b3f331bcd4873e52ca7708e178a276e7ebe3f864
Shubham Mehra <subhammehra4@gmail.com>
2021-04-26 13:34:01 +0530

    Sqrt

---
+++ Problems/Sqrt Implementation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int sqrt (int x)
+{
+    if (x == 0 || x == 1)
+        return x;
+
+    ll int start = 1, end = x, ans;
+
+    while (start <= end)
+    {
+        ll int mid = (start + end) / 2;
+
+        if (mid * mid == x)
+            return mid;
+
+        if (mid * mid < x)
+        {
+            start = mid + 1;
+            ans = mid;
+        }
+        else
+            end = mid - 1;
+    }
+
+    return ans;
+}
+
+
+
+int main()
+{
+    cout << sqrt (9767564789);
+    return 0;
+}
