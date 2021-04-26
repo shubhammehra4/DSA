c0dea5eca0b12aa3bd72ed88f354e4528988c097
Shubham Mehra <subhammehra4@gmail.com>
2021-04-26 20:23:18 +0530

    Greedy

---
+++ Algorithms/Greedy/Job_Sequencing.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Job
+{
+    string id;
+    int dead;
+    int profit;
+};
+
+bool comparison(Job a, Job b)
+{
+    return (a.profit > b.profit);
+}
+
+void printJobScheduling(Job arr[], int n, int total)
+{
+    sort(arr, arr + n, comparison);
+    vector<int> result;
+    vector<bool> slot(total, false);
+    int profit = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = min(total, arr[i].dead) - 1; j >= 0; j--)
+        {
+            if (!slot[j])
+            {
+                result.push_back(i);
+                slot[j] = true;
+                profit += arr[j].profit;
+                break;
+            }
+        }
+    }
+
+    for (auto i : result)
+        cout << arr[i].id << " ";
+    cout << "\nProfit: " << profit;
+}
+
+int main()
+{
+    Job arr[] = {{"j1", 3, 35}, {"j2", 4, 30}, {"j3", 4, 25}, {"j4", 2, 20}, {"j5", 3, 15}, {"j6", 1, 12}, {"j7", 2, 5}};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printJobScheduling(arr, n, 4);
+    return 0;
+}
