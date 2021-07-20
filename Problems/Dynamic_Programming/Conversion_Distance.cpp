37d208681c32706ef1856c2f39197c9bed7abb95
Shubham Mehra <shubhammehra4@gmail.com>
2021-07-20 12:53:20 +0530

    DP

--- Problems/Dynamic_Programming/Conversion_Distance.cpp
+++ Problems/Dynamic_Programming/Conversion_Distance.cpp
@@ -4,20 +4,22 @@ using namespace std;
 int getMinCostConversion(string s1, string s2)
 {
     int n = s1.length(), m = s2.length();
-    int dp[n][m];
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
     for (int i = 0; i <= n; i++)
+        dp[i][0] = i;
+
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = j;
+
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= m; j++)
+        for (int j = 1; j <= m; j++)
         {
-            if (i == 0)
-                dp[i][j] = j;
-            else if (j == 0)
-                dp[i][j] = i;
-            else if (s1[i - 1] == s2[j - 1])
+            if (s1[i - 1] == s2[j - 1])
                 dp[i][j] = dp[i - 1][j - 1];
             else
-                dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j]);
         }
     }
 
@@ -31,3 +33,18 @@ int main()
     getMinCostConversion(str1, str2);
     return 0;
 }
+
+// for (int i = 0; i <= n; i++)
+// {
+//     for (int j = 0; j <= m; j++)
+//     {
+// if (i == 0)
+//     dp[i][j] = j;
+// else if (j == 0)
+//     dp[i][j] = i;
+//         else if (s1[i - 1] == s2[j - 1])
+//             dp[i][j] = dp[i - 1][j - 1];
+//         else
+//             dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));
+//     }
+// }
