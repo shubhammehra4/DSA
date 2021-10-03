2da822305b5d892bea4041249ba04b2486d3825d
Shubham Mehra <shubhammehra4@gmail.com>
2021-10-03 23:22:39 +0530

    Merge

---
+++ Problems/Trees/BST_Merge.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+{
+    if (!root1 and !root2)
+        return NULL;
+
+    if (!root1)
+        return root2;
+
+    if (!root2)
+        return root1;
+
+    root1->val = root1->val + root2->val;
+    root1->left = mergeTrees(root1->left, root2->left);
+    root1->right = mergeTrees(root1->right, root2->right);
+
+    return root1;
+}
+
+int main()
+{
+    return 0;
+}
