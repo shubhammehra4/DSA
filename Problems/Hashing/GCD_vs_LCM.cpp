edbc6b03bdacc5f030c28b0c30e03889f4778457
Shubham Mehra <shubhammehra4@gmail.com>
2021-10-14 12:22:55 +0530

    GCD vs LCM

---
+++ Problems/Hashing/GCD_vs_LCM.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ll MOD = 998244353;
+double eps = 1e-12;
+#define INF 2e18
+
+int main()
+{
+
+    fast_cin();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        unordered_map<ll, ll> mp;
+        ll x;
+
+        forn(i, n)
+        {
+            cin >> x;
+            mp[x]++;
+        }
+
+        ll ans = 0;
+        for (auto x : mp)
+        {
+            if (mp.find(2 * x.first) != mp.end())
+                ans += mp[x.first] * mp[2 * x.first];
+        }
+
+        cout << ans << ln;
+    }
+
+    return 0;
+}
