bd70a9416428b81b1dbfb65b4aea17b8c13823ae
Shubham Mehra <shubhammehra4@gmail.com>
2021-10-15 14:00:18 +0530

    Trie

--- Data Structures/Trie.cpp
+++ Data Structures/Trie.cpp
@@ -1,74 +1,174 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
 using namespace std;
-const int ALPHABET_SIZE = 26;
 
-struct TrieNode
+// **** Space Effecient Approach
+class TrieNode
 {
-    struct TrieNode *children[ALPHABET_SIZE];
-    bool isEndOfWord;
+public:
+    char content;                // the char present at the node
+    bool isWordEnd;              // if the node is the end of a word
+    int shared;                  // the number of the node shared ,convenient to implement delete(string key)
+    vector<TrieNode *> children; // the children of the node
+
+    TrieNode() : content(' '), isWordEnd(false), shared(0) {}
+    TrieNode(char ch) : content(ch), isWordEnd(false), shared(0) {}
+
+    TrieNode *findChild(char ch)
+    {
+        if (!children.empty())
+        {
+            for (auto child : children)
+            {
+                if (child->content == ch)
+                    return child;
+            }
+        }
+        return nullptr;
+    }
+
+    ~TrieNode()
+    {
+        for (auto child : children)
+            delete child;
+    }
 };
 
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void)
+class Trie
 {
-    struct TrieNode *pNode = new TrieNode;
+private:
+    TrieNode *root;
 
-    pNode->isEndOfWord = false;
+public:
+    Trie() : root(new TrieNode()) {}
 
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        pNode->children[i] = NULL;
-
-    return pNode;
-}
-
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just
-// marks leaf node
-void insert(struct TrieNode *root, string key)
-{
-    struct TrieNode *pCrawl = root;
-
-    for (int i = 0; i < key.length(); i++)
+    /** Inserts a word into the trie */
+    void insert(string s)
     {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
-            pCrawl->children[index] = getNode();
+        if (search(s))
+            return;
 
-        pCrawl = pCrawl->children[index];
+        TrieNode *curr = root;
+        for (auto ch : s)
+        {
+            TrieNode *child = curr->findChild(ch);
+            if (child != nullptr)
+                curr = child;
+            else
+            {
+                TrieNode *newNode = new TrieNode(ch);
+                curr->children.push_back(newNode);
+                curr = newNode;
+            }
+            ++curr->shared;
+        }
+        curr->isWordEnd = true;
     }
 
-    pCrawl->isEndOfWord = true;
-}
-
-bool search(struct TrieNode *root, string key)
-{
-    struct TrieNode *pCrawl = root;
-
-    for (int i = 0; i < key.length(); i++)
+    /** Returns true if the word is in the trie */
+    bool search(string key)
     {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
-            return false;
-
-        pCrawl = pCrawl->children[index];
+        TrieNode *curr = root;
+        for (auto ch : key)
+        {
+            curr = curr->findChild(ch);
+            if (curr == nullptr)
+                return false;
+        }
+        return curr->isWordEnd == true;
     }
 
-    return (pCrawl->isEndOfWord);
-}
+    /** Returns true if there is any word in the trie that starts with the given prefix */
+    bool startsWith(string prefix)
+    {
+        TrieNode *curr = root;
+        for (auto ch : prefix)
+        {
+            curr = curr->findChild(ch);
+            if (curr == nullptr)
+                return false;
+        }
+        return true;
+    }
+    ~Trie()
+    {
+        delete root;
+    }
+};
 
-int main()
-{
-    string keys[] = {"the", "a", "there",
-                     "answer", "any", "by",
-                     "bye", "their"};
-    int n = sizeof(keys) / sizeof(keys[0]);
+// Time Effecient
 
-    struct TrieNode *root = getNode();
+// class TrieNode
+// {
+// public:
+//     TrieNode *children[26];
+//     bool isWordEnd;
 
-    for (int i = 0; i < n; i++)
-        insert(root, keys[i]);
+//     TrieNode()
+//     {
+//         isWordEnd = false;
+//         for (int i = 0; i < 26; i++)
+//             children[i] = nullptr;
+//     }
+// };
 
-    search(root, "the") ? cout << "Yes\n" : cout << "No\n";
-    search(root, "these") ? cout << "Yes\n" : cout << "No\n";
-    return 0;
-}
+// class Trie
+// {
+// private:
+//     TrieNode *root;
+
+// public:
+//     Trie() : root(new TrieNode()) {}
+
+//     void insert(string key)
+//     {
+//         if (search(key))
+//             return;
+
+//         TrieNode *pCrawl = root;
+
+//         for (auto ch : key)
+//         {
+//             int index = ch - 'a';
+//             if (!pCrawl->children[index])
+//                 pCrawl->children[index] = new TrieNode();
+
+//             pCrawl = pCrawl->children[index];
+//         }
+
+//         pCrawl->isWordEnd = true;
+//     }
+
+//     bool search(string key)
+//     {
+//         TrieNode *pCrawl = root;
+
+//         for (auto ch : key)
+//         {
+//             int index = ch - 'a';
+//             if (!pCrawl->children[index])
+//                 return false;
+
+//             pCrawl = pCrawl->children[index];
+//         }
+
+//         return (pCrawl->isWordEnd);
+//     }
+
+//     bool startsWith(string prefix)
+//     {
+//         TrieNode *node = root;
+//         for (char ch : prefix)
+//         {
+//             int index = ch - 'a';
+//             if (!node->children[index])
+//             {
+//                 return false;
+//             }
+//             node = node->children[index];
+//         }
+//         return true;
+//     }
+// };
